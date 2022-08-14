97109ee446c718c65ae0b9d27deb2e9d3866beb6
Han Jaehoon <hjhoon03@naver.com>
2022-08-14 20:00:06 +0900

    9184

---
+++ 9184.cpp
@@ -0,0 +1,43 @@
+// BOJ 9184
+#include <iostream>
+
+using namespace std;
+
+int result[21][21][21];
+
+int w(int, int, int);
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b, c;
+    while(1) {
+        cin >> a >> b >> c;
+        if(a == -1 && b == -1 && c == -1)
+            break;
+
+        cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << '\n';
+    }
+
+    return 0;
+}
+
+int w(int a, int b, int c) {
+    if(a <= 0 || b <= 0 || c <= 0)
+        return 1;
+
+    if(a > 20 || b > 20 || c > 20)
+        return w(20, 20, 20);
+
+    if(result[a][b][c] != 0)
+        return result[a][b][c];
+
+    if(a < b && b < c) {
+        result[a][b][c] =  w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+        return result[a][b][c];
+    }
+
+    result[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+    return result[a][b][c];
+}
