01ecef06fe5e964aebb39d61f45ced87facf4109
한재훈 <hjhoon03@naver.com>
2022-10-16 11:38:46 +0900

    solve 2751 with RadixSort

--- 2751.py
+++ 2751.py
@@ -1,5 +1,32 @@
-l = [int(input()) for i in range(int(input()))]
-l.sort()
+def sorting(lst, n):
+    sortingTable = [list() for i in range(10)]
 
-for i in l:
+    for i in lst:
+        sortingTable[i % (10 ** (n + 1)) // (10 ** n)].append(i)
+
+    sorted = list()
+    for i in sortingTable:
+        for j in i:
+            sorted.append(j)
+
+    return sorted
+
+nums = [int(input()) for i in range(int(input()))]
+
+positiveNums = list()
+negativeNums = list()
+
+for i in nums:
+    if i < 0:
+        negativeNums.append(-i)
+    else:
+        positiveNums.append(i)
+
+for i in range(7):
+    positiveNums = sorting(positiveNums, i)
+    negativeNums = sorting(negativeNums, i)
+
+for i in negativeNums[::-1]:
+    print(-i)
+for i in positiveNums:
     print(i)
